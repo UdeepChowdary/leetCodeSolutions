9dc8cdfb0c8aa7a65d42ed334c70956515da0c10
Naripeddi Udeep Chowdary <udeepchowdary@gmail.com>
2026-05-25 20:59:09 +0530

    Time: 1 ms (99.97%), Space: 42.9 MB (12.06%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int reverse(int x) {
+        int revNumber = 0;
+        int lastDigit = 0;
+        
+        while (x != 0) {
+            lastDigit = x % 10;
+            
+            if (revNumber > Integer.MAX_VALUE / 10) {
+                return 0;
+            }
+            if (revNumber < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+            revNumber = (revNumber * 10) + lastDigit;
+            x = x / 10;
+        }
+        return revNumber;
+    }
+}
